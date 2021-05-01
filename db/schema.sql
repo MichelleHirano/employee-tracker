02b041861aaec5a4cc85cc441c18edf50448224f
MHirano <michelle@hiranohomes.com>
2021-05-01 12:35:33 -0700

    data input into schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,26 @@ CREATE DATABASE employees;
 USE employees;
 
 --employee
+CREATE TABLE employee(
+    id INTEGER AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+);
+
+
+-- role
+CREATE TABLE role(
+    id INTEGER AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10,2),
+    department_id INTEGER
+);
+
+
+--role
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+);
---
+++ db/seeds.sql
@@ -0,0 +1,35 @@
+
+--employees
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Ronald', 'Firbank', 1, 1),
+    ('Virginia', 'Woolf', 1, 1),
+    ('Piers', 'Gaveston', 1, 0),
+    ('Charles', 'LeRoi', 2, 1),
+    ('Katherine', 'Mansfield', 2, 1),
+    ('Dora', 'Carrington', 3, 0),
+    ('Edward', 'Bellamy', 3, 0),
+    ('Montague', 'Summers', 3, 1),
+    ('Octavia', 'Butler', 3, 1),
+    ('Unica', 'Zurn', NULL, 1);
+
+  --role
+  INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Engineer', 50000, 3),
+    ('Deparment Manager', 100000, 1),
+    ('Analyst', 60000, 3),
+    ('Engineer', 50000, 3),
+    ('CEO', 200000, 1),
+    ('Head Engineer', 150000, 1),
+    ('Senior Engineer', 100000, 2),
+    ('Senior Engineer', 1000000, 2),
+    ('Analyst', 600000, 3),
+    ('CFO', 180000, 1);
+
+--department
+  INSERT INTO department (department_name)
+VALUES
+    ('Engineering'),
+    ('Management'),
+    ('Data');
